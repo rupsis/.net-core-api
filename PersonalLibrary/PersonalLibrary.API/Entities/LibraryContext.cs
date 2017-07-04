3838deef8529cd54307b671dc1949318df5a3d77
Kratos <nrupsis@yahoo.com>
2017-07-04 12:49:01 -0400

    fluched out entities more, added LobraryContext

---
+++ PersonalLibrary/PersonalLibrary.API/Entities/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalLibrary.API.Entities
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            :base(options)
+        {
+         
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
