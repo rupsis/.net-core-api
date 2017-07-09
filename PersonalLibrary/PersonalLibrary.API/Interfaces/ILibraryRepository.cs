abb6cd6703e324ea12afe586677ee636bb04fa61
Kratos <nrupsis@yahoo.com>
2017-07-08 23:42:45 -0400

    added library repository

---
+++ PersonalLibrary/PersonalLibrary.API/Interfaces/ILibraryRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PersonalLibrary.API.Entities;
+
+namespace PersonalLibrary.API.Interfaces
+{
+    public interface ILibraryRepository
+    {
+        IEnumerable<Author> GetAuthors();
+        Author GetAuthor(Guid authorId);
+        IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
+        void AddAuthor(Author author);
+        void DeleteAuthor(Author author);
+        void UpdateAuthor(Author author);
+        bool AuthorExists(Guid authorId);
+        IEnumerable<Book> GetBooksForAuthor(Guid authorId);
+        Book GetBookForAuthor(Guid authorId, Guid bookId);
+        void AddBookForAuthor(Guid authorId, Guid bookId);
+        void UpdateBookForAuthor(Book book);
+        void DeleteBookForAuthor(Book book);
+        bool Save();
+    }
+}
---
+++ PersonalLibrary/PersonalLibrary.API/Repositories/LibraryRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PersonalLibrary.API.Entities;
+using PersonalLibrary.API.Interfaces;
+
+namespace PersonalLibrary.API.Repositories
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        public void AddAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBookForAuthor(Guid authorId, Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthorExists(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBookForAuthor(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author GetAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookForAuthor(Guid authorId, Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetBooksForAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBookForAuthor(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
