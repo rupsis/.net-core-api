17e17b3e77cdfb6795acf84d2b7f8f849655f9d5
Kratos <nrupsis@yahoo.com>
2017-07-10 23:01:38 -0400

    added controller for authors

---
+++ PersonalLibrary/PersonalLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PersonalLibrary.API.Repositories;
+
+namespace PersonalLibrary.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private LibraryRepository _libraryRepository;
+
+        public AuthorsController(LibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
