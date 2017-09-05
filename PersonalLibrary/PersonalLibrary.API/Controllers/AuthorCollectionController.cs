1b29054567065e32a080541457bdbf6ce9ce4a41
Kratos <nrupsis@yahoo.com>
2017-09-05 01:52:22 -0400

    added model binding helper to return comma seperated list of ids for author collection creation

--- PersonalLibrary/PersonalLibrary.API/Controllers/AuthorCollectionController.cs
+++ PersonalLibrary/PersonalLibrary.API/Controllers/AuthorCollectionController.cs
@@ -44,9 +44,33 @@ namespace PersonalLibrary.API.Controllers
                 throw new Exception("Creating an author failed on save");
             }
 
+            var authorCollectionToReturn = Mapper.Map<IEnumerable<Core.Models.Author>>(authorEntities);
 
-            // Need to return location header and 201 status
-            return Ok();
+            // create a comma seperated string of the collection ids
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+
+            // return the route of the newly created collection
+            return CreatedAtRoute("GetAuthorCollection", new { ids = idsAsString }, authorCollectionToReturn);
+        }
+
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult GetAuthorCollection(
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))]IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntites = _libraryRepository.GetAuthors(ids);
+
+            if(ids.Count() != authorEntites.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = Mapper.Map<IEnumerable<Core.Models.Author>>(authorEntites);
+            return Ok(authorsToReturn);
         }
     }
 }
---
+++ PersonalLibrary/PersonalLibrary.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace PersonalLibrary.API.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            // Binding only works on enumerable types
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            // Get inputted values
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            // if value is null or whitespace, return null
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            // converts string type to guid
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];
+            var converter = TypeDescriptor.GetConverter(elementType);
+
+            // convert value list into enumerable type
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(
+                x => converter.ConvertFromString(x.Trim())).ToArray();
+
+            // create array of model values 
+            var typedValues = Array.CreateInstance(elementType, values.Length);
+            values.CopyTo(typedValues, 0);
+            bindingContext.Model = typedValues;
+
+            // return success
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
